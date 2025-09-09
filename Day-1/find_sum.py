62ee0e07c071d6e3b2cf0a0cd89e4d4412cf4f5c
lasyamacs08@gmail.com <lasyamacs08@gmail.com>
2025-09-09 16:51:01 +0530

    Adding 09092025

---
+++ Day-1/find_sum.py
@@ -0,0 +1,14 @@
+#Sum of two numbers
+def find_sum(first,second):
+    result = first+second
+    return result
+
+#Bhanu
+res = find_sum(10.5, 20.5) 
+#store in file
+print(res)
+
+#Bhaskar
+res2 = find_sum(5,6) 
+#save into db
+print(res2)
