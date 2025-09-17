f681db62a9a0a7018991e955a32b49771bbf3610
LasyaMoravineni <124281083+LasyaMoravineni@users.noreply.github.com>
2025-09-17 09:28:49 +0530

    Update db.py

--- Project/hms/app/db.py
+++ Project/hms/app/db.py
@@ -2,15 +2,19 @@
 db.py - Database initialization
 """
 
-from app.models import db
-from app.config import config
+from hms.app.config import config
+from flask_sqlalchemy import SQLAlchemy
+
+db = SQLAlchemy()
 
 
 def init_db(application):
+    
     """Initialize database with Flask app"""
-    application.config["SQLALCHEMY_DATABASE_URI"] = config["DB_URL"]
-    application.config["SQLALCHEMY_ECHO"] = True
-    db.init_app(application)
+    if "sqlalchemy" not in application.extensions:
+        application.config["SQLALCHEMY_DATABASE_URI"] = config["DB_URL"]
+        application.config["SQLALCHEMY_ECHO"] = True
+        db.init_app(application)
 
     with application.app_context():
         db.create_all()
