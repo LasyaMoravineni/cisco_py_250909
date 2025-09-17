1d26269d81a8411c878c644dc1c46ddda0f39422
lasyamacs08@gmail.com <lasyamacs08@gmail.com>
2025-09-17 14:31:29 +0530

    Updates project

--- Project/hms/app/logger.py
+++ Project/hms/app/logger.py
@@ -1,9 +1,13 @@
+"""
+logger.py - Centralized logging configuration for HMS
+"""
+
 import logging
 
 logging.basicConfig(
     filename="hospital_app_logs.log",
     level=logging.INFO,
-    format="%(asctime)s [%(levelname)s] %(message)s"
+    format="%(asctime)s [%(levelname)s] %(message)s",
 )
 
 logger = logging.getLogger(__name__)
